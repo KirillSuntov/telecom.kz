c2a027d45ee2936bce77e284e15bcf77f1ef0434
kirill_zu@mail.ru <kirill_zu@mail.ru>
2022-07-26 09:38:41 +0600

    password+  screenshot

--- src/test/java/telecom/pages/MobilePage.java
+++ src/test/java/telecom/pages/MobilePage.java
@@ -1,4 +1,5 @@
 package telecom.pages;
+
 import io.qameta.allure.Step;
 import org.junit.Assert;
 import telecom.testconfig.BaseSetings;
@@ -25,10 +26,10 @@ public class MobilePage {
     public void CheckBody() {
         $(withText("Мобильное приложение")).click();
         pause(5000);
-        CheckImg("mobile__convenient","img/main/mobile/convenient_image.webp");
-        CheckImg("mobile__balance","img/main/mobile/balance_image.webp");
-        CheckImg("mobile__questions","img/main/mobile/questions__image.webp");
-        CheckDownloadLinks() ;
+        CheckImg("mobile__convenient", "img/main/mobile/convenient_image.webp");
+        CheckImg("mobile__balance", "img/main/mobile/balance_image.webp");
+        CheckImg("mobile__questions", "img/main/mobile/questions__image.webp");
+        CheckDownloadLinks();
 
     }
 
@@ -36,20 +37,32 @@ public class MobilePage {
     public void CheckDownloadLinks() {
 
 //        System.out.println($(byId("downloadLinks")).$(byTagName("img")).getAttribute("alt"));
-       $(byId("downloadLinks")).$(byTagName("img")).getAttribute("src").equals("img/main/mobile/google_play.svg");
+        $(byId("downloadLinks")).$(byTagName("img")).getAttribute("src").equals("img/main/mobile/google_play.svg");
         $(byId("downloadLinks")).$(byTagName("img")).getAttribute("alt").equals("google_play");
-        if ($(byId("downloadLinks")).$(byTagName("img")).getSize().height<50){
+        if ($(byId("downloadLinks")).$(byTagName("img")).getSize().height < 50) {
+            screenshot("123");
+            try {
+                screenshot1("Скрин последней страницы");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             Assert.fail("Изображение не отображается");
         }
 //        $(byClassName("footer")).$$(byClassName("footer-list")).get(g).$(withText(elementText)).scrollTo().shouldHave(attribute("href", href));
-        $(byId("downloadLinks")).$$(byAttribute("target","_blank")).get(0).shouldHave(attribute("href", "https://play.google.com/store/apps/details?id=kz.telecom.app"));
+        $(byId("downloadLinks")).$$(byAttribute("target", "_blank")).get(0).shouldHave(attribute("href", "https://play.google.com/store/apps/details?id=kz.telecom.app"));
 
         $(byId("downloadLinks")).$$(byTagName("img")).get(1).getAttribute("src").equals("img/main/mobile/app_store.svg");
         $(byId("downloadLinks")).$$(byTagName("img")).get(1).getAttribute("alt").equals("app_store");
-        if ($(byId("downloadLinks")).$$(byTagName("img")).get(1).getSize().height<50){
+        if ($(byId("downloadLinks")).$$(byTagName("img")).get(1).getSize().height < 50) {
+            screenshot("123");
+            try {
+                screenshot1("Скрин последней страницы");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             Assert.fail("Изображение не отображается");
         }
-        $(byId("downloadLinks")).$$(byAttribute("target","_blank")).get(1).shouldHave(attribute("href", "https://apps.apple.com/kz/app/telecom-kz/id1545816005"));
+        $(byId("downloadLinks")).$$(byAttribute("target", "_blank")).get(1).shouldHave(attribute("href", "https://apps.apple.com/kz/app/telecom-kz/id1545816005"));
 
 
     }
@@ -60,10 +73,9 @@ public class MobilePage {
 //        System.out.println($(byClassName(className)).$(byTagName("img")).getAttribute("src"));
 //        System.out.println(baseUrl+srcUrl);
 
-        if ( $(byClassName(className)).$(byTagName("img")).getAttribute("src").equals("https://telecom.kz/"+srcUrl)){
+        if ($(byClassName(className)).$(byTagName("img")).getAttribute("src").equals("https://telecom.kz/" + srcUrl)) {
 //            System.out.println("Корректная ссылка на изображение");
-        }
-        else{
+        } else {
 
             screenshot("123");
             try {
@@ -74,10 +86,9 @@ public class MobilePage {
             Assert.fail("Изменена ссылка на изображение");
         }
 
-        if ($(byClassName(className)).$(byTagName("img")).getSize().height>100){
+        if ($(byClassName(className)).$(byTagName("img")).getSize().height > 100) {
 //            System.out.println("Изображение отображается");
-        }
-        else{
+        } else {
 
             screenshot("123");
             try {
