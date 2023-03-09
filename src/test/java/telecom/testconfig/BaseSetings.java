10942e58c6a6603d3fc3b25041d5f0530fc96bef
kirill_zu@mail.ru <kirill_zu@mail.ru>
2023-03-09 15:55:45 +0600

    AuthCCTV fix

--- src/test/java/telecom/testconfig/BaseSetings.java
+++ src/test/java/telecom/testconfig/BaseSetings.java
@@ -76,7 +76,7 @@ public class BaseSetings {
         Configuration.timeout= 80000;
         Configuration.pageLoadTimeout=80000;
         Configuration.browserSize = "1300x1080";
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.holdBrowserOpen = false;
 
 
