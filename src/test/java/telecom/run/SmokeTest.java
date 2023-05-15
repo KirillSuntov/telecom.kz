663b37c4624e6e7f1a169f3c6ee684c1be945b93
kirill_zu@mail.ru <kirill_zu@mail.ru>
2023-05-15 16:39:50 +0600

    CheckMobileDetails()

--- src/test/java/telecom/run/SmokeTest.java
+++ src/test/java/telecom/run/SmokeTest.java
@@ -90,7 +90,8 @@ public class SmokeTest extends BaseSetings {
         adm.logInLk2(login, password);
 //        adm.lkPage().CheckLk1();
 //
-        adm.detailsPage().CheckDetails();
+        adm.detailsPage().
+        CheckMobileDetails();
     }
 
     @Test
