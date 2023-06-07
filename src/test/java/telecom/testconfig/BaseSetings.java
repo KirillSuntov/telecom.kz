676a46899248048ff40926f4a8de944f6f99037a
kirill_zu@mail.ru <kirill_zu@mail.ru>
2023-06-07 11:29:12 +0600

    headless headless headless

--- src/test/java/telecom/testconfig/BaseSetings.java
+++ src/test/java/telecom/testconfig/BaseSetings.java
@@ -82,7 +82,7 @@ public class BaseSetings {
         Configuration.pageLoadTimeout=80000;
         Configuration.browserSize = "1300x1080";
 
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.holdBrowserOpen = false;
 
 
